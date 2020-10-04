077e59703602e0f1cd1a15eb8832de464b3943f2
Derek Vasconcelos <derek@cepetro.unicamp.br>
2020-10-04 12:43:02 -0300

    Applied floatpos patch

--- config.def.h
+++ config.def.h
@@ -29,6 +29,9 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
 
+static int floatposgrid_x           = 5;        /* float grid columns */
+static int floatposgrid_y           = 5;        /* float grid rows */
+
 typedef struct {
 	const char *name;
 	const void *cmd;
@@ -50,14 +53,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance     title           tags mask  iscentered  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,        NULL,           0,         0,          1,          0,           0,        -1 },
-	{ "Firefox", NULL,        NULL,           1 << 8,    0,          0,          0,          -1,        -1 },
-	{ "St",      NULL,        NULL,           0,         0,          0,          1,           0,        -1 },
-	{ NULL,      NULL,        "Event Tester", 0,         0,          0,          0,           1,        -1 }, /* xev */
-	{ NULL,      "spterm",    NULL,           SPTAG(0),  0,          1,          1,           1,        -1 },
-	{ NULL,      "spfm",      NULL,           SPTAG(1),  0,          1,          1,           1,        -1 },
-	{ NULL,      "keepassxc", NULL,           SPTAG(2),  0,          0,          0,           1,        -1 },
+	/* class     instance     title           tags mask  iscentered  isfloating  isterminal  noswallow  floatpos  monitor */
+	{ "Gimp",    NULL,        NULL,           0,         0,          1,          0,           0,        NULL,     -1 },
+	{ "Firefox", NULL,        NULL,           1 << 8,    0,          0,          0,          -1,        NULL,     -1 },
+	{ "St",      NULL,        NULL,           0,         0,          0,          1,           0,        NULL,     -1 },
+	{ NULL,      NULL,        "Event Tester", 0,         0,          0,          0,           1,        NULL,     -1 }, /* xev */
+	{ NULL,      "spterm",    NULL,           SPTAG(0),  0,          1,          1,           1,        NULL,     -1 },
+	{ NULL,      "spfm",      NULL,           SPTAG(1),  0,          1,          1,           1,        NULL,     -1 },
+	{ NULL,      "keepassxc", NULL,           SPTAG(2),  0,          0,          0,           1,        NULL,     -1 },
 };
 
 /* layout(s) */
@@ -163,6 +166,44 @@ static Key keys[] = {
 	{ MODKEY,                       XK_y,      togglescratch,  {.ui = 0 } },
 	{ MODKEY,                       XK_u,      togglescratch,  {.ui = 1 } },
 	{ MODKEY,                       XK_x,      togglescratch,  {.ui = 2 } },
+	/* Client position is limited to monitor window area */
+	{ Mod4Mask,                     XK_u,      floatpos,       {.v = "-26x -26y" } }, // ↖
+	{ Mod4Mask,                     XK_i,      floatpos,       {.v = "  0x -26y" } }, // ↑
+	{ Mod4Mask,                     XK_o,      floatpos,       {.v = " 26x -26y" } }, // ↗
+	{ Mod4Mask,                     XK_j,      floatpos,       {.v = "-26x   0y" } }, // ←
+	{ Mod4Mask,                     XK_l,      floatpos,       {.v = " 26x   0y" } }, // →
+	{ Mod4Mask,                     XK_m,      floatpos,       {.v = "-26x  26y" } }, // ↙
+	{ Mod4Mask,                     XK_comma,  floatpos,       {.v = "  0x  26y" } }, // ↓
+	{ Mod4Mask,                     XK_period, floatpos,       {.v = " 26x  26y" } }, // ↘
+	/* Absolute positioning (allows moving windows between monitors) */
+	{ Mod4Mask|ControlMask,         XK_u,      floatpos,       {.v = "-26a -26a" } }, // ↖
+	{ Mod4Mask|ControlMask,         XK_i,      floatpos,       {.v = "  0a -26a" } }, // ↑
+	{ Mod4Mask|ControlMask,         XK_o,      floatpos,       {.v = " 26a -26a" } }, // ↗
+	{ Mod4Mask|ControlMask,         XK_j,      floatpos,       {.v = "-26a   0a" } }, // ←
+	{ Mod4Mask|ControlMask,         XK_l,      floatpos,       {.v = " 26a   0a" } }, // →
+	{ Mod4Mask|ControlMask,         XK_m,      floatpos,       {.v = "-26a  26a" } }, // ↙
+	{ Mod4Mask|ControlMask,         XK_comma,  floatpos,       {.v = "  0a  26a" } }, // ↓
+	{ Mod4Mask|ControlMask,         XK_period, floatpos,       {.v = " 26a  26a" } }, // ↘
+	/* Resize client, client center position is fixed which means that client expands in all directions */
+	{ Mod4Mask|ShiftMask,           XK_u,      floatpos,       {.v = "-26w -26h" } }, // ↖
+	{ Mod4Mask|ShiftMask,           XK_i,      floatpos,       {.v = "  0w -26h" } }, // ↑
+	{ Mod4Mask|ShiftMask,           XK_o,      floatpos,       {.v = " 26w -26h" } }, // ↗
+	{ Mod4Mask|ShiftMask,           XK_j,      floatpos,       {.v = "-26w   0h" } }, // ←
+	{ Mod4Mask|ShiftMask,           XK_k,      floatpos,       {.v = "800W 800H" } }, // ·
+	{ Mod4Mask|ShiftMask,           XK_l,      floatpos,       {.v = " 26w   0h" } }, // →
+	{ Mod4Mask|ShiftMask,           XK_m,      floatpos,       {.v = "-26w  26h" } }, // ↙
+	{ Mod4Mask|ShiftMask,           XK_comma,  floatpos,       {.v = "  0w  26h" } }, // ↓
+	{ Mod4Mask|ShiftMask,           XK_period, floatpos,       {.v = " 26w  26h" } }, // ↘
+	/* Client is positioned in a floating grid, movement is relative to client's current position */
+	{ Mod4Mask|Mod1Mask,            XK_u,      floatpos,       {.v = "-1p -1p" } }, // ↖
+	{ Mod4Mask|Mod1Mask,            XK_i,      floatpos,       {.v = " 0p -1p" } }, // ↑
+	{ Mod4Mask|Mod1Mask,            XK_o,      floatpos,       {.v = " 1p -1p" } }, // ↗
+	{ Mod4Mask|Mod1Mask,            XK_j,      floatpos,       {.v = "-1p  0p" } }, // ←
+	{ Mod4Mask|Mod1Mask,            XK_k,      floatpos,       {.v = " 0p  0p" } }, // ·
+	{ Mod4Mask|Mod1Mask,            XK_l,      floatpos,       {.v = " 1p  0p" } }, // →
+	{ Mod4Mask|Mod1Mask,            XK_m,      floatpos,       {.v = "-1p  1p" } }, // ↙
+	{ Mod4Mask|Mod1Mask,            XK_comma,  floatpos,       {.v = " 0p  1p" } }, // ↓
+	{ Mod4Mask|Mod1Mask,            XK_period, floatpos,       {.v = " 1p  1p" } }, // ↘
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
